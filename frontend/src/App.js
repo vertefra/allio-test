e0b9454edcf3c894e0fe6dcf3bf9a0871344c292
vertefra <verte.fra@gmail.com>
2020-10-25 12:40:43 -0400

    show page structured

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen.js'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Container>
         <main className="py-3">
           <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen} />
         </main>
       </Container>
       <Footer />
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,20 +1,29 @@
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Allio</Navbar.Brand>
+          <LinkContainer to="/">
+            <Nav.Link>
+              <Navbar.Brand>Allio</Navbar.Brand>
+            </Nav.Link>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fas fa-shopping-cart"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i className="fas fa-shopping-user"></i>Login
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fas fa-shopping-cart"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <i className="fas fa-shopping-user"></i>Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
