43970fe27d39a0b684f64b3c3649f22dd782d439
vertefra <verte.fra@gmail.com>
2020-11-09 09:26:24 -0500

    login page and login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen.js'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
       <Header />
       <Container>
         <main className="py-3">
-          <Route path="/" component={HomeScreen} exact />
+          <Route path="/login" component={LoginScreen} />
           <Route path="/product/:id" component={ProductScreen} />
           <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" component={HomeScreen} exact />
         </main>
       </Container>
       <Footer />
