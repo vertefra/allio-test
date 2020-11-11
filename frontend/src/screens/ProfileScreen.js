fab90586a56396fe2a4f0f7113d2afd32048aac4
vertefra <verte.fra@gmail.com>
2020-11-11 09:05:38 -0500

    added update profile form and connected with backend

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -3,10 +3,10 @@ import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 
-import { getUserDetails } from '../actions/userActions'
+import { getUserDetails, updateUserProfile } from '../actions/userActions'
 import Loader from '../components/Loader'
 
-const ProfileScreen = ({ location, history }) => {
+const ProfileScreen = ({ history }) => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -16,8 +16,10 @@ const ProfileScreen = ({ location, history }) => {
 
   const userDetails = useSelector((state) => state.userDetails)
   const userLogin = useSelector((state) => state.userLogin)
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
 
-  const { user, loading, error } = userDetails
+  const { success, error } = userUpdateProfile
+  const { user, loading } = userDetails
   const { userInfo } = userLogin
 
   useEffect(() => {
@@ -37,12 +39,11 @@ const ProfileScreen = ({ location, history }) => {
 
   const submitHandler = (e) => {
     e.preventDefault()
-    // DISPATCH REGISTER
 
     if (password !== confirmPassword) {
       setMessage('Password do not match')
     } else {
-      // DISPATCH UPDATE PROFILE
+      dispatch(updateUserProfile({ id: user._id, name, email, password }))
     }
   }
 
@@ -52,6 +53,7 @@ const ProfileScreen = ({ location, history }) => {
         <h2>User Profile</h2>
         {message && <Message variant="danger">{message}</Message>}
         {error && <Message variant="danger">{error}</Message>}
+        {success && <Message variant="success">Profile Updated</Message>}
         {loading && <Loader />}
         <Form onSubmit={submitHandler}>
           <Form.Group controlId="name">
