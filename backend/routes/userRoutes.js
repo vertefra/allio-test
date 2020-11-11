fcd675980a1f9065ce5600c56a1b98b59585af32
vertefra <verte.fra@gmail.com>
2020-11-11 07:47:48 -0500

    put route for user profile

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,13 +3,17 @@ import {
   authUser,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userControllers.js'
 import protect from '../middleware/authMiddleware.js'
 const userRouter = express.Router()
 
 userRouter.route('/').post(registerUser)
 userRouter.route('/login').post(authUser)
-userRouter.route('/profile').get(protect, getUserProfile)
+userRouter
+  .route('/profile')
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile)
 userRouter.route('/register').post(registerUser)
 
 export default userRouter
